307c40f6d072fc0f9e02bf01f579024d0c8b7664
Сергій Заморило <zamorylo@i.ua>
2024-03-18 20:33:48 +0200

    ,

--- reg.js
+++ reg.js
@@ -1,49 +1,50 @@
 import "./src/scss/reg.js";
 import axios from "axios";
-const inputs={
-    emailInputs:document.querySelector("#EmailInput"),
-    passwordInput:document.querySelector("#PasswordInput"),
-    repassword:document.querySelector('#Re-enterPassword'),
-    errorInput:document.querySelector(`#error`),
-    loginInput:document.querySelector(`#LoginInput`)
+const FormEl={
+    login:document.querySelector("#LoginInput"),
+    password:document.querySelector("#PasswordInput"),
+    email:document.querySelector("#EmailInput"),
+    name:document.querySelector("#nameInput"),
+    signUp:document.querySelector("#SingUp"),
+    error:document.querySelector("#error"),
+    repassword:document.querySelector("#Re-enterPassword")
 }
-const SingUpButton=document.querySelector("#SingUp");
-SingUpButton.addEventListener("click",()=>{
-    let succcsesfull=true;
-    let ParamNow="";
-    console.log(candidate)
-    for(const param in candidate){
-        if(candidate[param].length == 0){
-            succcsesfull = false;
-            ParamNow=param
-        }
-    }
-    if(!succcsesfull){
-        inputs.errorInput.innerHTML=`Param ${ParamNow} has been inccorect`
+FormEl.signUp.addEventListener("click",()=>{
+    let points=0
+    if(FormEl.login.value.length!=0){
+        points += 1
     } else{
-        const checkPassword = candidate.password == candidate.repassword;
-        if(!checkPassword){
-            inputs.errorInput.innerHTML='Check your password or Re-password'
-        } else{
-            let isInclude = false
-            users.forEach((person)=>{
-                if (person.email==candidate.email){
-                    isInclude = true 
-                }
-            }) 
-            if(isInclude){
-                inputs.errorInput.innerHTML='Check your email'
-            } else{
-                axios.post("http://localhost:4000/user/create",{
-                    login:inputs.loginInput.value,
-                    password:inputs.passwordInput.value,
-                    email:inputs.emailInputs.value,
-                    name:inputs.nameInput.value
-                }).then((res)=>{
-                    localStorage.setItem("Token",userToken)
-                    window.location.href=`/login.html`
-                })
-            }
-        }
+        FormEl.error.innerHTML=`Write your login`
     }
-});
+    if(FormEl.password.value.length!=0){
+        points+=1
+    }else{
+        FormEl.error.innerHTML=`Write your password`
+    }
+    if(FormEl.email.value.length!=0){
+        points+=1
+    }else{
+        FormEl.error.innerHTML=`Write your email`
+    }
+    if(FormEl.name.value.length!=0){
+        points+=1
+    }else{
+        FormEl.error.innerHTML=`Write your name`
+    }
+    if(FormEl.password.value==FormEl.repassword.value){
+        points+=1
+    } else{
+        FormEl.error.innerHTML=`Write your correct password`
+    }
+    if(points==5){
+        axios.post("http://localhost:4000/user/create",{
+            login:FormEl.login.value,
+            password:FormEl.password.value,
+            email:FormEl.email.value,
+            name:FormEl.name.value
+        }).then((res)=>{
+            localStorage.setItem("Token",res.data.userToken)
+            window.location.href=`/profile.html`
+        })
+    }
+})
